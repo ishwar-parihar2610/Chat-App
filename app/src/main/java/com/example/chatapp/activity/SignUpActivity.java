47467fc30cf59edfa7efc7ec7d7d2acb3ccd304e
ishwar parihar <paruharishwer26102000@gmail.com>
2021-09-07 11:01:03 +0530

    chat App

--- app/src/main/java/com/example/chatapp/activity/SignUpActivity.java
+++ app/src/main/java/com/example/chatapp/activity/SignUpActivity.java
@@ -10,6 +10,8 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.util.Base64;
 import android.util.Patterns;
 import android.view.View;
@@ -17,20 +19,32 @@ import android.widget.Toast;
 
 import com.example.chatapp.R;
 import com.example.chatapp.databinding.ActivitySignUpBinding;
+import com.example.chatapp.utilities.Constant;
+import com.example.chatapp.utilities.PreferenceManager;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.regex.Pattern;
 
 public class SignUpActivity extends AppCompatActivity {
     ActivitySignUpBinding binding;
     String encodedImage;
+    private PreferenceManager preferenceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up);
+        preferenceManager=new PreferenceManager(getApplicationContext());
         binding.textViewSign.setOnClickListener(v -> startActivity(new Intent(SignUpActivity.this, SignInActivity.class)));
+        binding.layoutImage.setOnClickListener(v->{
+            Intent intent=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            pickImage.launch(intent);
+        });
         binding.buttonSignUp.setOnClickListener(v -> {
             if (isValidSignUpDetails()) {
                 signUp();
@@ -43,6 +57,27 @@ public class SignUpActivity extends AppCompatActivity {
     }
 
     private void signUp() {
+        loading(true);
+        FirebaseFirestore database=FirebaseFirestore.getInstance();
+        HashMap<String,Object> user=new HashMap<>();
+        user.put(Constant.KEY_NAME,binding.inputName.getText().toString());
+        user.put(Constant.KEY_EMAIL,binding.inputEmail.getText().toString());
+        user.put(Constant.KEY_PASSWORD,binding.inputPassword.getText().toString());
+        user.put(Constant.KEY_IMAGE,encodedImage);
+        database.collection(Constant.KEY_COLLECTION_USERS).add(user).addOnSuccessListener(DocumentReference ->{
+        loading(false);
+        preferenceManager.putBoolean(Constant.KEY_IS_SIGNED_IN,true);
+        preferenceManager.putString(Constant.KEY_USER_ID,DocumentReference.getId());
+        preferenceManager.putString(Constant.KEY_NAME,binding.inputName.getText().toString());
+        preferenceManager.putString(Constant.KEY_IMAGE,encodedImage);
+        Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+
+        }).addOnFailureListener(exception ->{
+        loading(false);
+        showToast(exception.getMessage());
+        });
 
     }
     private String encodedImage(Bitmap bitmap){
@@ -64,6 +99,7 @@ public class SignUpActivity extends AppCompatActivity {
                    Bitmap bitmap= BitmapFactory.decodeStream(inputStream);
                    binding.profileImage.setImageBitmap(bitmap);
                    binding.addImageTv.setVisibility(View.GONE);
+                   encodedImage=encodedImage(bitmap);
                } catch (Exception e) {
                    e.printStackTrace();
                }
